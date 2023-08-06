51b24b5e7d1a3dfc44a8c187d0af83719fd1e44b
Noer <36747322+happyhourxd@users.noreply.github.com>
2023-08-06 04:24:14 -0400

    sentax?

--- updater.lua
+++ updater.lua
@@ -2,7 +2,6 @@ if not fs.exists("json") then
     shell.run("pastebin get 4nRg9CHU json")
 end
 
-
 os.loadAPI("json")
 
 local githubUsername = "happyhourxd"
@@ -14,23 +13,6 @@ local response = http.get(apiUrl)
 local jsonResponse = response.readAll()
 response.close()
 local treeData = json.decode(jsonResponse)
-
-for _, entry in ipairs(treeData.tree) do
-    if entry.type == "blob" then
-        -- Access the file path directly from entry.path
-        local filePath = fs.combine("/", entry.path)
-
-        -- Delete the existing file if it exists
-        if fs.exists(filePath) then
-            fs.delete(filePath)
-        end
-
-        -- Download the new file content directly from the GitHub Raw URL
-        _G.arg = {filePath}
-        downloadFile(filePath)
-    end
-end
-
 
 function downloadFile(filePath)
     local response = http.get(string.format("https://raw.githubusercontent.com/happyhourxd/MinecraftTurtles/main/%s", filePath))
@@ -45,5 +27,21 @@ function downloadFile(filePath)
         print("Downloaded:", filePath)
     else
         print("Error: Unable to download file:", filePath)
+    end
+end
+
+for _, entry in ipairs(treeData.tree) do
+    if entry.type == "blob" then
+        -- Access the file path directly from entry.path
+        local filePath = fs.combine("/", entry.path)
+
+        -- Delete the existing file if it exists
+        if fs.exists(filePath) then
+            fs.delete(filePath)
+        end
+
+        -- Download the new file content directly from the GitHub Raw URL
+        _G.arg = {filePath}
+        downloadFile(filePath)
     end
 end
