c364de2f99c8ae830d747ce0e0e5365c7da47030
Noer <36747322+happyhourxd@users.noreply.github.com>
2023-08-06 02:48:17 -0400

    delete new files before re-aquiring them

--- updater.lua
+++ updater.lua
@@ -1,15 +1,20 @@
 os.loadAPI("json")
 local treeData = json.decode(http.get(string.format("https://api.github.com/repos/happyhourxd/MinecraftTurtles/get/trees/main?recursive=1")).readAll())
 
-
 for _, entry in ipairs(treeData.tree) do
     if entry.type == "blob" then
         -- Access the file path directly from entry.path
+        local filePath = fs.combine("/", entry.path)
+
+        -- Delete the existing file if it exists
+        if fs.exists(filePath) then
+            fs.delete(filePath)
+        end
+
+        -- Download and write the new file content
         local fileUrl = entry.url
         local fileResponse = http.get(fileUrl)
         if fileResponse then
-            -- Combine the root folder with the entry.path
-            local filePath = fs.combine("/", entry.path)
             local file = fs.open(filePath, "w")
             file.write(fileResponse.readAll())
             file.close()
