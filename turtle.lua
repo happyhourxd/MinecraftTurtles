77784e530f5bbcb3e31fd6b99ff95ef40b09ebd3
Noer <36747322+happyhourxd@users.noreply.github.com>
2023-08-06 22:54:26 -0400

    made the miner work mayhaps

--- turtle.lua
+++ turtle.lua
@@ -16,6 +16,10 @@ H = read()
 NeededFuel = (L * W * H) + L + W + H
 
 RightOrLeft = 0
+direction = .5 -- in pi radians
+
+
+print(L, " ", W, " ", H)
 
 for i = 0,H,1
 do    
@@ -31,13 +35,23 @@ do
                 turtle.turnRight()
                 turtle.forward()
                 turtle.turnRight()
+                direction - .5
             else
                 RightOrLeft = 0
                 turtle.turnLeft()
                 turtle.forward()
                 turtle.turnLeft()
+                direction + .5
             end
         end
+        if not ((W % 2) && (j == W)) then
+            print(direction, "this should be .5")
+            turtle.turnLeft()
+            turtle.turnLeft()
+        else
+            print(direction, "this shouldn't be .5 i think?")
+            turtle.turnRight()
+            turtle.turnRight()
     end
     if not (i == H) then
         turtle.up()
