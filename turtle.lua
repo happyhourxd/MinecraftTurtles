0636e65b3400767dad43a6d8cef6d9cc213c5b39
Noer <36747322+happyhourxd@users.noreply.github.com>
2023-08-07 22:17:18 -0400

    another test *sigh*

--- turtle.lua
+++ turtle.lua
@@ -23,34 +23,24 @@ direction = .5 -- in pi radians
 print(L, " ", W, " ", H)
 
  
-for j = 1,W,1
+for (j = 1,W,1)
 do
-    for k = 1,L,1
+    for(k = 1,L,1)
     do
         turtle.forward()
-        print(j, " is j ",k, " is k ")
     end
-    L = L - 1 -- this is a test
-    if not (j == W) then
+    L= L - 1
+    if (j < W) then
         if (RightOrLeft == 0) then
             RightOrLeft = 1
             turtle.turnRight()
             turtle.forward()
             turtle.turnRight()
-            direction = direction - .5
         else
             RightOrLeft = 0
             turtle.turnLeft()
             turtle.forward()
-            turtle.turnLeft()
-            direction = direction + .5
+            turtle.turnLeft() 
         end
-    end
-    if not ((W % 2) and (j == W)) then
-        print(direction, "this should be .5")
-        turtle.turnLeft()
-    else
-        print(direction, "this shouldn't be 0 i think?")
-        turtle.turnRight()
     end
 end
