d0c05afc90c0c8be6f7be862bcb398ddbe6d75dc
Noer <36747322+happyhourxd@users.noreply.github.com>
2023-08-13 16:34:36 -0400

    more testing off return

--- blockManipulation/returnWOGPS.lua
+++ blockManipulation/returnWOGPS.lua
@@ -8,7 +8,8 @@ for i = 1, H - 1, 1 do
     turtle.down()
 end
 
-if (not ((W % 2) == 0) and not ((H % 2) == 0)) then
+if ((not ((W % 2) == 0)) and (not ((H % 2) == 0))) then -- W = 5, H = 1
+    print("odd Width, and odd hidth")
     for i = 1, L - 1,1 
     do
         turtle.forward()
@@ -19,20 +20,19 @@ if (not ((W % 2) == 0) and not ((H % 2) == 0)) then
         turtle.forward()
     end
     turtle.turnLeft()
-elseif (((W % 2) == 0) and (not(H % 2) == 0)) then
-    print
+elseif (((W % 2) == 0) and (not(H % 2) == 0)) then -- W = 4, H = 1
+    --todo?
+    print("even width odd highth")
     turtle.turnLeft()
     for i = 1, L - 1, 1 do
         turtle.forward()
     end
     turtle.turnLeft()
-elseif (((H % 2) == 0) and (not(W % 2) ==0)) then
-    turtle.turnRight()
-    turtle.turnRight()
-    turtle.turnRight()
-    turtle.turnRight()
-    turtle.turnRight()
-else
+elseif (((H % 2) == 0) and (not(W % 2) ==0)) then -- W  = 5, H = 2
+    --todo
+    print("odd width even highth")
+else --done W = 4, H = 2
+    print("even width even highth")
     turtle.turnRight()
     for i = 1, W - 1,1
     do
