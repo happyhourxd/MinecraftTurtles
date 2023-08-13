5cc36b97422c609034c9dfea8ad4287bfca40368
Noer <36747322+happyhourxd@users.noreply.github.com>
2023-08-13 18:20:18 -0400

    made replace?

--- blockManipulation/replace.lua
+++ blockManipulation/replace.lua
@@ -1,3 +1,84 @@
+-- User input for mine size
 L = _G.arg[1]
 W = _G.arg[2]
 H = _G.arg[3]
+
+L = tonumber(L)
+W = tonumber(W)
+H = tonumber(H)
+
+
+-- Calculate needed fuel
+NeededFuel = (L * W * H) + L + W + H
+print(turtle.getFuelLevel())
+while (NeededFuel > turtle.getFuelLevel()) do
+    print("Enter ", (NeededFuel - turtle.getFuelLevel()) / 10, "coal")
+    read()
+    turtle.refule(1)
+end
+
+--calculate needed blocks
+amountOfBlocksNeeded = (L * W * H)
+amtOfBlocks = 0
+while (amountOfBlocksNeeded > amtOfBlocks)
+    for i = 1, 16, 1 do
+        amtOfBlocks = amtOfBlocks + turtle.getItemCount(i)
+    end
+    if (amountOfBlocksNeeded > amtOfBlocks) then
+        print("enter ", amountOfBlocksNeeded - amtOfBlocks, " blocks")
+        read()
+    end
+end
+
+-- Initialize direction variables
+RightOrLeft = 1
+direction = 0.5 -- in pi radians
+
+
+turtle.up()
+a = 1
+-- Loop through each layer of the mine (height)
+for i = 1, H do
+    -- Loop through each row (width)
+    for j = 1, W do
+        -- Loop through each column (length)
+        for k = 2, L do
+            if (turtle.getItemCount(a) >= 1) then
+                turtle.placeDown()
+            else
+                a = a + 1
+                turtle.select(a)
+                turtle.placeDown()
+            end
+            turtle.forward()
+        end        
+        -- Handle turning and changing direction
+        if j < W then
+            if RightOrLeft == 0 then
+                RightOrLeft = 1
+                turtle.turnRight()
+                turtle.forward()
+                turtle.turnRight()
+                direction = direction - 1
+            else
+                RightOrLeft = 0
+                turtle.turnLeft()
+                turtle.forward()
+                turtle.turnLeft()
+                direction = direction + 1
+            end
+        end
+    end
+    
+    -- Move up a layer
+    if i < H then
+        turtle.up()
+        height = height + 1
+    end
+    
+    -- Turn around after completing a layer
+    turtle.turnRight()
+    turtle.turnRight()
+    direction = direction + 1
+end
+turtle.down()
